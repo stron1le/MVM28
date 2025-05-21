11cac7d79ae74d7fce3ee2c190ae1982e0b3c6ad
CaimNidhoggr <57159458+stron1le@users.noreply.github.com>
2025-05-20 21:40:43 -0400

    Standard Player Camera Implemented
    Player camera has now been enhanced in a few ways.
    1. The player can now rotate the camera around their character.
    2. This rotation is capped vertically. So the camera will never do a flip and get unaligned.
    3. The camera can be moved with both controller AND mouse.

--- mvm28/Scripts/Globals.gd
+++ mvm28/Scripts/Globals.gd
@@ -4,6 +4,7 @@ const SLIPPERY_TERRAIN=0x1;
 const GAME_MODE_ACTION=0x0;
 const GAME_MODE_MENU=0x1;
 const GAME_MODE_CUTSCENE=0x2;
+
 var paused=false;
 var gameMode=GAME_MODE_ACTION;
 func get_terrain(terrain_name:String):
--- mvm28/TwistPivot.gd
+++ mvm28/TwistPivot.gd
@@ -1,13 +1,25 @@
 extends Node3D
 @export var target:Node;
-const CAMERA_TURN_SPEED=30;
+const CAMERA_TURN_SPEED=deg_to_rad(180);
+var mouseHidden:bool=true;
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED);
 func _physics_process(delta):
 	if (target!=null):
 		global_position=target.global_position;
 	var cam_input = Input.get_vector("TurnCameraLeftController","TurnCameraRightController","TurnCameraDownController","TurnCameraUpController");
 	if (cam_input!=Vector2.ZERO):
 		var cam_target_angle=CAMERA_TURN_SPEED*cam_input.x*delta
+		print(cam_target_angle);
 		transform.basis=transform.basis.rotated(transform.basis.y,cam_target_angle);
 		cam_target_angle=CAMERA_TURN_SPEED*cam_input.y*delta;
-		$PitchPivot.transform.basis=$PitchPivot.transform.basis.rotated(transform.basis.x,cam_target_angle)
-	
+		$PitchPivot.transform.basis=$PitchPivot.transform.basis.rotated($PitchPivot.transform.basis.x,cam_target_angle)
+		$PitchPivot.rotation_degrees.x=clamp($PitchPivot.rotation_degrees.x,-60,60);
+func _unhandled_input(event):
+	if ((event is InputEventMouseMotion) and mouseHidden):
+		rotation_degrees.y-=event.relative.x*0.5;
+		$PitchPivot.rotation_degrees.x-=event.relative.y*0.2;
+		$PitchPivot.rotation_degrees.x=clamp($PitchPivot.rotation_degrees.x,-60,60);
+	elif (Input.is_action_just_pressed("ui_cancel")):
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE if mouseHidden else Input.MOUSE_MODE_CAPTURED);
+		mouseHidden=!mouseHidden;
